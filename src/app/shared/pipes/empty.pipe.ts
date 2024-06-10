9c651fc9003004ac8b0eca5befc14691ecef20eb
Jorinde Reijnierse <jorinde.reijnierse@usmedia.nl>
2024-06-10 18:01:59 +0200

    Fix: Ignore 0 on empty pipe because it's a valid value

--- src/app/shared/pipes/empty.pipe.ts
+++ src/app/shared/pipes/empty.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'empty' })
 export class EmptyPipe implements PipeTransform {
     public transform(value: undefined | null | string | number): string | number {
-        if (value === '' || value === undefined || value === null || value === 0) {
+        if (value === '' || value === undefined || value === null) {
             return '-';
         } else {
             return value;
