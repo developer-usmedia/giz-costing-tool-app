73f5887fc8de5e6d80570eb3e709ebd3ea150618
Joost Passon <joost.passon@usmedia.nl>
2024-11-28 18:24:46 +0100

    feature: GIZ-19 Imported IKB needs to use the capped value

--- src/app/api/models/worker-remuneration.model.ts
+++ src/app/api/models/worker-remuneration.model.ts
@@ -2,11 +2,5 @@ export interface WorkerRemuneration {
     baseWage: number;
     bonuses: number;
     ikb: number;
-    ikbHousing: number;
-    ikbFood: number;
-    ikbTransport: number;
-    ikbHealthcare: number;
-    ikbChildcare: number;
-    ikbChildEducation: number;
     total: number;
 }
